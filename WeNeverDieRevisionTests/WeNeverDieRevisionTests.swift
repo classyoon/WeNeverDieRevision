700294fed90a5d7890b2e3695be68771e9277962
classyoon <96508736+classyoon@users.noreply.github.com>
2024-10-23 14:42:13 -0400

    Got swift testing to work, super easy.

--- WeNeverDieRevisionTests/WeNeverDieRevisionTests.swift
+++ WeNeverDieRevisionTests/WeNeverDieRevisionTests.swift
@@ -7,11 +7,18 @@
 
 import Testing
 @testable import WeNeverDieRevision
-
+class MathDude {
+    func getSimple()->Int{
+        return 1
+    }
+}
 struct WeNeverDieRevisionTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        var test = MathDude()
+        #expect(test.getSimple() == 1)
     }
+    
 
 }
